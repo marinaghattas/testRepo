638084c4c92febb1bd325e4c676be95714ac4e90
Marina Ghattas <marina.ghattas@F1006-11-34159.bmcc.cuny.edu>
2015-10-05 17:07:36 -0400

    more in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var red;
+
 function setup(){
     red = color(255,0,0);
+    blue = color = (0,0,255);
 }
 function draw(){
-
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mousseY);
+    }
 }
